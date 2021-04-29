c235743fbd5a1001a01fe646718ed710124d6491
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-04-29 23:21:58 +0200

    Change naming for the Matrix class to be more convenient

--- Lab2/metrices/Matrix.java
+++ Lab2/metrices/Matrix.java
@@ -3,10 +3,10 @@ package metrices;
 import java.lang.System;
 import java.lang.Exception;
 
-public class Matrix implements Addable<int[][]> {
+public class Matrix implements Addable<Matrix> {
     protected int[][] numbers; // the number in the matrix
-    protected int m; // number of rows
-    protected int n; // number of cols
+    protected int rows; // number of rows
+    protected int cols; // number of cols
 
     static final class MultiplicationException extends Exception {
         public String errorMessage;
@@ -20,33 +20,45 @@ public class Matrix implements Addable<int[][]> {
         }
     }
 
-    public Matrix(int m, int n) {
-        this.m = m;
-        this.n = n;
-        numbers = new int[m][n];
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        numbers = new int[rows][cols];
     }
 
-    public int[][] add(int[][] nums) {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                numbers[i][j] += nums[i][j];
+    public Matrix add(Matrix mat) {
+        if (mat.cols != this.cols || mat.rows != this.rows) {
+            System.out.print(
+                    "Addition must be between 2 metrices that have the same order (i.e., same number of rows and columns)\n");
+        } else {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    this.numbers[i][j] += mat.numbers[i][j];
+                }
             }
         }
-        return numbers;
+
+        return this;
     }
 
-    public boolean setNumbers(int[] rows) {
-        if (rows.length != this.m * this.n)
+    public boolean setNumbers(int[] flattened) {
+        // Size of the flattened array = n*m
+        if (flattened.length != this.rows * this.cols)
             return false;
 
-        for (int i = 0; i < m; i++) {
-            System.arraycopy(rows, (i * n), numbers[i], 0, n);
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(flattened, (i * cols), numbers[i], 0, cols); // src, srcPos, dest, destPos, length
         }
 
         return true;
     }
 
     public void print() {
+        /*
+         * It prints the elements of the matrix each row in a separate line and each row
+         * elements are separated by spaces.
+         * 
+         */
         for (int[] row : numbers) {
             for (int x : row) {
                 System.out.print(x + " ");
@@ -56,13 +68,19 @@ public class Matrix implements Addable<int[][]> {
         System.out.print("\n");
     }
 
-    public int[][] transpose() {
-        int[][] transposed = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+    public void transpose() {
+        int[][] transposed = new int[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
                 transposed[i][j] = numbers[j][i];
             }
         }
-        return transposed;
+        // Swap number of cols and rows using Bitwise XOR
+        // For more details:
+        // https://www.geeksforgeeks.org/swap-two-variables-in-one-line-in-c-c-python-php-and-java/
+        rows = rows ^ cols ^ (cols = rows);
+
+        // Numbers array should carry its transpose
+        this.numbers = transposed;
     }
 }
