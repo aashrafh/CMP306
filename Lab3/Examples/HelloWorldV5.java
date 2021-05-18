026571d418bc5fd1d699708c0cc36c03e6b21640
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-05-18 23:34:15 +0200

    Setting up mpj express

--- Lab3/Examples/HelloWorldV5.java
+++ Lab3/Examples/HelloWorldV5.java
@@ -1,18 +1,13 @@
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import mpi.*;
+
 public class HelloWorldV5 {
     public static void main(String args[]) throws Exception {
-          MPI.Init(args);
+        MPI.Init(args);
         int me = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        System.out.println("Hi from <"+me+">"+" and the size is "+size);
+        System.out.println("Hi from <" + me + ">" + " and the size is " + size);
         MPI.Finalize();
-		 
+
     }
 
-    
 }
