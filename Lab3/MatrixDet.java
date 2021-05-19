be94825d71b0a11ef3d7e411c87dac8181e58003
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-05-19 04:28:44 +0200

    Gather the results from all processes

--- Lab3/MatrixDet.java
+++ Lab3/MatrixDet.java
@@ -8,14 +8,18 @@ public class MatrixDet {
         MPI.Init(args);
         int root = 0;
         int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
         int[] recvbuf = new int[1];
+        int[] sendbuf;
+        int[] detbuf;
+        char[] message = new char[30];
 
         if (rank == root) {
             System.out.printf("I'm the root, my rank is: %d\n", rank);
 
             // Initialize the matrix
             Matrix mat = new Matrix(3, 3, Arrays.copyOfRange(args, 3, args.length));
-            mat.print();
+            // mat.print();
 
             ByteArrayOutputStream outStream = new ByteArrayOutputStream();
             ObjectOutput out = null;
@@ -40,6 +44,23 @@ public class MatrixDet {
                 }
             }
 
+            // ========= Gather the partial determinants from all processes =========
+            sendbuf = new int[size];
+            detbuf = new int[size];
+
+            // Gathers all data from all processes in the world including the root
+            MPI.COMM_WORLD.Gather(sendbuf, 0, 1, MPI.INT, detbuf, 0, 1, MPI.INT, root);
+
+            System.out.println("The received determinants from all processes including itself: ");
+            for (int i = 0; i < size; i++) {
+                System.out.println(detbuf[i]);
+            }
+
+            // Terminate the processes
+            message = "You can exit now".toCharArray();
+            System.out.println(message);
+            MPI.COMM_WORLD.Bcast(message, 0, message.length, MPI.CHAR, root);
+
         } else {
             // Recieving the matrix
             MPI.COMM_WORLD.Bcast(recvbuf, 0, 1, MPI.INT, root);
@@ -52,8 +73,8 @@ public class MatrixDet {
             try {
                 in = new ObjectInputStream(inStream);
                 Matrix mat = (Matrix) in.readObject();
-                System.out.printf("Process %d: I recieved the matrix: \n", rank);
-                mat.print();
+                // System.out.printf("Process %d: I recieved the matrix: \n", rank);
+                // mat.print();
             } finally {
                 try {
                     if (in != null) {
@@ -63,7 +84,19 @@ public class MatrixDet {
                     System.out.println(ex);
                 }
             }
-            System.out.println("Process " + rank + " exiting ...");
+
+            int[] dummy = new int[size];
+            sendbuf = new int[1];
+
+            sendbuf[0] = (rank + 1) * 10;
+
+            // Send the new determinant to the root
+            MPI.COMM_WORLD.Gather(sendbuf, 0, 1, MPI.INT, dummy, 0, 1, MPI.INT, root);
+
+            // Recieve last message and terminate
+            MPI.COMM_WORLD.Bcast(message, 0, 30, MPI.CHAR, root);
+            String Msg = new String(message);
+            System.out.println("Process " + rank + " exiting ...with message: " + Msg);
         }
 
         // Terminates MPI execution environment
