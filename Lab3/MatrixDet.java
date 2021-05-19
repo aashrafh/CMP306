b44ab4cc23e28b38381147959d404e6775a65c2b
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-05-19 02:46:15 +0200

    Broadcast the matrix class to all processes

--- Lab3/MatrixDet.java
+++ Lab3/MatrixDet.java
@@ -1,11 +1,13 @@
 import mpi.*;
+import java.io.*;
 
 public class MatrixDet {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         // Initializes the MPI execution environment
         MPI.Init(args);
         int root = 0;
         int rank = MPI.COMM_WORLD.Rank();
+        int[] recvbuf = new int[1];
 
         if (rank == root) {
             System.out.printf("I'm the root, my rank is: %v\n", rank);
@@ -13,10 +15,28 @@ public class MatrixDet {
             // Initialize the matrix
             Matrix mat = new Matrix(3, 3);
             mat.initializeMatrix();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            ObjectOutput out = null;
 
             // Broadcasting the matrix to all processes
             System.out.prinln("Root: I will broadcast the matrix to all processes");
-            MPI.COMM_WORLD.Bcast(message, 0, message.length, MPI.CHAR, 0);
+            try {
+                out = new ObjectOutputStream(stream);
+                out.writeObject(mat);
+                out.flush();
+
+                byte[] matBytes = stream.toByteArray();
+                recvbuf[0] = matBytes.length;
+
+                MPI.COMM_WORLD.Bcast(recvbuf, 0, 1, MPI.INT, root);
+                MPI.COMM_WORLD.Bcast(matBytes, 0, matBytes.length, MPI.BYTE, root);
+            } finally {
+                try {
+                    stream.close();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
 
         } else {
 
