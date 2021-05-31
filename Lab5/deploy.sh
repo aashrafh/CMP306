daef12c002b63f510a8771ef71d27642fe01c08b
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-05-31 20:45:30 +0200

    Add the html and xml files

--- Lab5/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-/home/aashrafh/Desktop/apache-tomcat-7.0.109/bin/shutdown.sh
-/home/aashrafh/Desktop/apache-tomcat-7.0.109/bin/startup.sh
-cp /home/aashrafh/Desktop/CMP/APT/CMP306/Lab5/SevletsV10/bin/ServletMatrix.class /home/aashrafh/Desktop/apache-tomcat-7.0.109/webapps/ROOT/WEB-INF/classes/
